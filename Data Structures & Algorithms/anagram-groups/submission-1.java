174854605a876661db1b20f468cea6055500a325
Aditi Gupta <aditi251gupta@gmail.com>
2026-08-29 22:58:22 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for(String s: strs) {
+            // char ch[] = s.toCharArray();
+            // Arrays.sort(ch);
+            // String sortedS = new String(ch);
+
+            int count[] = new int[26];
+            for(char ch: s.toCharArray()) {
+                count[ch- 'a']++;
+            }
+            String sortedS = Arrays.toString(count);
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+
+        return new ArrayList<>(res.values());
+    }
+}
